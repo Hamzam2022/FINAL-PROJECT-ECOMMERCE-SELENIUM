b31f462d76c0429be0a230e30a6e92052dbbdbf2
hamza <mograbi.ha@gmail.com>
2022-06-23 12:54:57 +0300

    verifying incorrect values error masseges

--- test_automationEcommerceWebsiteFireFox.py
+++ test_automationEcommerceWebsiteFireFox.py
@@ -92,3 +92,29 @@ def test_verifyMandatoryFieldsErrorMessage(driver):
     element1 = driver.find_element(By.CSS_SELECTOR, ".error .error-tip")
     driver.execute_script("arguments[0].click();", element1)
     assert element1.text == "Please enter an email address."
+
+def test_verifyIncorrectValuesErrorMessage(driver):
+    element = driver.find_element(By.CSS_SELECTOR, ".sui_icon_nav_me_24px")
+    driver.execute_script("arguments[0].click();", element)
+    time.sleep(3)
+    driver.find_element(By.CSS_SELECTOR,
+                        ".page-signup__emailLoginItem > .input-area-email .S-input__inner").click()
+    driver.find_element(By.CSS_SELECTOR,
+                        ".page-signup__emailLoginItem > .input-area-email .S-input__inner").send_keys("tfakott")
+
+    driver.find_element(By.CSS_SELECTOR,
+                        ".page-signup__emailLoginItem > .input-area-password .S-input__inner").click()
+    driver.find_element(By.CSS_SELECTOR,
+                        ".page-signup__emailLoginItem > .input-area-password .S-input__inner").send_keys("h123")
+
+    driver.find_element(By.CSS_SELECTOR,
+                        ".page-login__stylePreference:nth-child(4) .S-checkbox:nth-child(2) .S-checkbox__input-inner").click()
+    driver.find_element(By.CSS_SELECTOR, ".login-btn:nth-child(6) span").click()
+    time.sleep(5)
+    element1 = driver.find_element(By.CSS_SELECTOR, ".error .error-tip")
+    driver.execute_script("arguments[0].click();", element1)
+    assert element1.text == "The email you entered is invalid. Please check your email and try again."
+
+    element1 = driver.find_element(By.CSS_SELECTOR, ".normal-red > p:nth-child(1)")
+    driver.execute_script("arguments[0].click();", element1)
+    assert element1.text == "· 8 characters minimum"
